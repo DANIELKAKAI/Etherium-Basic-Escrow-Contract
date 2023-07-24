3bc727adbf3617ba5d20d4aa12ac050566da0e7e
dankakai <daniel.kakai@twiga.com>
2023-07-24 19:46:55 +0300

    initial commit

---
+++ escrow.js
@@ -0,0 +1,285 @@
+//const Web3 = require('web3');
+const contractAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_buyer",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_seller",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_arbiter",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [],
+        "name": "ApprovedByBuyer",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [],
+        "name": "ApprovedBySeller",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "depositor",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "PaymentDeposited",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "refundRecipient",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "Refunded",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "amount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "approveByBuyer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "approveBySeller",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "arbiter",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "buyer",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "buyerApproved",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "currentState",
+        "outputs": [
+            {
+                "internalType": "enum Escrow.State",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "refundBuyer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "seller",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "sellerApproved",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+const escrowContractAddress = "0xd1ce5ca1273d923Ec228AffFb324198d26615954";
+//const privateKey = 'YOUR_PRIVATE_KEY';
+
+const web3 = new Web3("http://localhost:7545");
+
+//const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+//web3.eth.accounts.wallet.add(account);
+//web3.eth.defaultAccount = account.address;
+
+const escrowContract = new web3.eth.Contract(contractAbi, escrowContractAddress);
+
+async function depositFunds(address = null, etherAmount = null) {
+
+    if (!address && !etherAmount) {
+        address = document.getElementById("address").value ? document.getElementById("address").value : null;
+        etherAmount = document.getElementById("amount").value ? document.getElementById("amount").value : null;
+    }
+    const depositAmount = web3.utils.toWei(etherAmount, 'ether');
+
+    try {
+        const depositTx = await escrowContract.methods.deposit().send({
+            from: address,
+            value: depositAmount,
+        });
+
+        console.log('Transaction hash:', depositTx.transactionHash);
+    } catch (error) {
+        console.error('Error depositing funds:', error);
+    }
+}
+
+async function approveByBuyer(address) {
+    try {
+        const approvalTx = await escrowContract.methods.approveByBuyer().send({
+            from: address,
+        });
+
+        console.log('Transaction hash:', approvalTx.transactionHash);
+    } catch (error) {
+        console.error('Error approving by buyer:', error);
+    }
+}
+
+async function approveBySeller(address) {
+    try {
+        const approvalTx = await escrowContract.methods.approveBySeller().send({
+            from: address,
+        });
+
+        console.log('Transaction hash:', approvalTx.transactionHash);
+    } catch (error) {
+        console.error('Error approving by seller:', error);
+    }
+}
+
+async function refundBuyer(address) {
+    try {
+        const refundTx = await escrowContract.methods.refundBuyer().send({
+            from: address,
+        });
+
+        console.log('Transaction hash:', refundTx.transactionHash);
+    } catch (error) {
+        console.error('Error refunding the buyer:', error);
+    }
+}
+
+async function checkAmount() {
+    const balance = await escrowContract.methods.amount().call();
+    console.log("Amount:", balance);
+}
+
+async function checkStatus() {
+    const status = await escrowContract.methods.currentState().call();
+    console.log("Status:", status);
+}
+
+async function addressBalance(address) {
+    const balance = await web3.eth.getBalance(address);
+    console.log("Balance: ", balance);
+}
+
+//checkAmount();
+//depositFunds("0x3dA4ebD27ccafaA1285C4a7242e33b96a88EEB4E", "1");
+//checkAmount();
+
+//addressBalance("0xcb0a2c74d6E9d3D61279a0a8d8f9f1e8d4b07142");
+//addressBalance("0xDb306B4F524F502dDDe54d057d88f4e03Dbc7abC");
+//addressBalance("0x3dA4ebD27ccafaA1285C4a7242e33b96a88EEB4E");
+
+//checkAmount();
---
+++ escrow.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Escrow {
+    address public buyer;
+    address public seller;
+    address public arbiter;
+    uint256 public amount;
+    bool public buyerApproved;
+    bool public sellerApproved;
+
+    enum State {
+        AWAITING_PAYMENT,
+        AWAITING_DELIVERY,
+        COMPLETE,
+        REFUNDED
+    }
+    State public currentState;
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "Only the buyer can call this function");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only the seller can call this function");
+        _;
+    }
+
+    modifier onlyArbiter() {
+        require(
+            msg.sender == arbiter,
+            "Only the arbiter can call this function"
+        );
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(currentState == _state, "Invalid state");
+        _;
+    }
+
+    event PaymentDeposited(address depositor, uint256 amount);
+    event ApprovedByBuyer();
+    event ApprovedBySeller();
+    event Refunded(address refundRecipient, uint256 amount);
+
+    constructor(address _buyer, address _seller, address _arbiter) {
+        buyer = _buyer;
+        seller = _seller;
+        arbiter = _arbiter;
+        currentState = State.AWAITING_PAYMENT;
+    }
+
+    function deposit() external payable inState(State.AWAITING_PAYMENT) {
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        require(msg.sender == buyer, "Only the buyer can deposit funds");
+
+        amount = msg.value;
+        currentState = State.AWAITING_DELIVERY;
+
+        emit PaymentDeposited(msg.sender, msg.value);
+    }
+
+    function approveByBuyer()
+        external
+        onlyBuyer
+        inState(State.AWAITING_DELIVERY)
+    {
+        buyerApproved = true;
+        emit ApprovedByBuyer();
+
+        if (sellerApproved) {
+            currentState = State.COMPLETE;
+            // Transfer funds to the seller
+            (bool success, ) = seller.call{value: amount}("");
+            require(success, "Failed to transfer funds to the seller");
+        }
+    }
+
+    function approveBySeller()
+        external
+        onlySeller
+        inState(State.AWAITING_DELIVERY)
+    {
+        sellerApproved = true;
+        emit ApprovedBySeller();
+
+        if (buyerApproved) {
+            currentState = State.COMPLETE;
+            // Transfer funds to the seller
+            (bool success, ) = seller.call{value: amount}("");
+            require(success, "Failed to transfer funds to the seller");
+        }
+    }
+
+    function refundBuyer()
+        external
+        onlyArbiter
+        inState(State.AWAITING_DELIVERY)
+    {
+        currentState = State.REFUNDED;
+        // Refund the funds to the buyer
+        (bool success, ) = buyer.call{value: amount}("");
+        require(success, "Failed to refund the buyer");
+
+        emit Refunded(buyer, amount);
+    }
+}
